d8185ab3138eb2f4e33a0c599e9322846985d1dd
Peder Wiig Aalen <peder.wiig@skyetec.no>
2024-08-13 13:51:49 +0200

    fix typo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ const Navbar = () => {
     <>
       <div className="navbar bg-base-100">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-xl">Firesafe</a>
         </div>
         <div className="flex-none">
           <button className="btn btn-square btn-ghost">
