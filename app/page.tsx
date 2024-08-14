961de70f574eb05131c4fd49e11e6a53119de85f
Erik Dymbe <erik.dymbe@skyetec.no>
2024-08-14 10:18:50 +0200

    temporary fix: route landing page to selector

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import { categoryTree } from "./categories";
 
 export default function Home() {
+  const router = useRouter();
+  router.push(`/selector/${categoryTree.id}`);
   return (
     <main className="flex flex-col">
       <a className="link" href={`/selector/${categoryTree.id}`}>
