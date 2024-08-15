0556426a52c20ce7c4166650586b02d164e6463e
Peder Wiig Aalen <peder.wiig@skyetec.no>
2024-08-15 09:26:13 +0200

    add pwa support

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-};
+import withSerwistInit from "@serwist/next";
 
-export default nextConfig;
+const withSerwist = withSerwistInit({
+  // Note: This is only an example. If you use Pages Router,
+  // use something else that works, such as "service-worker/index.ts".
+  swSrc: "app/sw.ts",
+  swDest: "public/sw.js",
+  disable: false, // process.env.NODE_ENV === "development",
+});
+
+export default withSerwist({
+  output: "export",
+});
