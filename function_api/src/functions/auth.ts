53f62d6b312ca3873df1ca22e67d72fc3cee754d
Peder Wiig Aalen <peder.wiig@skyetec.no>
2024-08-21 13:29:35 +0200

    reenable github gateway

--- function_api/src/functions/auth.ts
+++ function_api/src/functions/auth.ts
@@ -1,11 +1,33 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import { ClientCredentials, ResourceOwnerPassword, AuthorizationCode } from "simple-oauth2";
+
+export const config = {
+  client: {
+    id: process.env.OAUTH_CLIENT_ID,
+    secret: process.env.OAUTH_CLIENT_SECRET,
+  },
+  auth: {
+    tokenHost: "https://github.com",
+    tokenPath: "/login/oauth/access_token",
+    authorizePath: "/login/oauth/authorize",
+  },
+};
 
 export async function auth(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
   context.log(`Http function processed request for url "${request.url}"`);
+  const client = new AuthorizationCode(config);
+  const authorizationUri = client.authorizeURL({
+    redirect_uri: process.env.REDIRECT_URL,
+    scope: "repo,user",
+  });
 
-  const name = request.query.get("name") || (await request.text()) || "world";
-
-  return { body: `Hello from auth, ${name}!` };
+  return {
+    status: 301,
+    headers: {
+      Location: authorizationUri,
+    },
+    jsonBody: { authorizationUri },
+  };
 }
 
 app.http("auth", {
--- function_api/src/functions/callback.ts
+++ function_api/src/functions/callback.ts
@@ -1,11 +1,48 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import { ClientCredentials, ResourceOwnerPassword, AuthorizationCode } from "simple-oauth2";
+import { config } from "./auth";
 
 export async function callback(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-  context.log(`Http function processed request for url "${request.url}"`);
+  const client = new AuthorizationCode(config);
+  const code = request.query.get("code");
+  const originPattern = process.env.ORIGIN || "";
+  let oauthProvider = "github";
+  let message, content;
+  try {
+    const accessToken = await client.getToken({ code: code, redirect_uri: process.env.REDIRECT_URL });
+    message = "success";
+    content = {
+      token: accessToken.token.access_token,
+      provider: oauthProvider,
+    };
+  } catch (error) {
+    context.error(`Access Token Error "${request.url}"`, error.message);
+    message = "error";
+    content = JSON.stringify(error);
+  }
+  const script = `
+  <script>
+  (function() {
+    function recieveMessage(e) {
+      console.log("recieveMessage %o", e)
+      if (!e.origin.match(${JSON.stringify(originPattern)})) {
+        console.log('Invalid origin: %s', e.origin);
+        return;
+      }
 
-  const name = request.query.get("name") || (await request.text()) || "world";
-
-  return { body: `Hello from callback function` };
+      window.opener.postMessage(
+        'authorization:github:${message}:${JSON.stringify(content)}',
+        e.origin
+      )
+    }
+    window.addEventListener("message", recieveMessage, false)
+    // Start handshare with parent
+    console.log("Sending message: %o", "${oauthProvider}")
+    window.opener.postMessage("authorizing:${oauthProvider}", "*")
+  })()
+  </script>`;
+  context.log(script);
+  return { body: script, headers: { "Content-Type": "text/html" } };
 }
 
 app.http("callback", {
